b004f25e5ca6770f4106b01a25260cc7ab4da541
jooyeop <royalmansu@gmail.com>
2022-11-16 16:13:01 +0900

    resolution

--- high-definition_resolution.py
+++ high-definition_resolution.py
@@ -15,10 +15,8 @@ base_directory = '../dataset/Image Super Resolution - Unsplash'
 hires_folder = os.path.join(base_directory, 'high res') # high resolution images를 저장한 폴더
 lowres_folder = os.path.join(base_directory, 'low res') # low resolution images를 저장한 폴더
 
-batch_size = 4
+data = pd.read_csv("../input/image-super-resolution-from-unsplash/Image Super Resolution - Unsplash/image_data.csv") # image_data.csv 파일을 읽어옴
+data['low_res'] = data['low_res'].apply(lambda x: os.path.join(lowres_folder,x)) # low resolution images의 경로를 저장 
+data['high_res'] = data['high_res'].apply(lambda x: os.path.join(hires_folder,x)) # high resolution images의 경로를 저장
 
-image_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-mask_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-
-
-train_hiresimage_generator = image_datagen.flow_from_dataframe
+atch_size = 4
